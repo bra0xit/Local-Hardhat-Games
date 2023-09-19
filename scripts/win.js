445430ef100833e0fd1fd4eca8cba92106ff27af
Bra0xit <ax.evander@gmail.com>
2023-09-19 07:44:36 +0200

    init commit

--- scripts/win.js
+++ scripts/win.js
@@ -1,5 +1,5 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
+const gameAddr = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 const contractName = "Game1";
 
 async function main() {
@@ -8,6 +8,8 @@ async function main() {
 
     // do whatever you need to do to win the game here:
     const tx = await game.win();
+    await tx.wait(); //make sure to always include these --> its best practice for making sure transactions are included in a block before you proceed with other logic, i.e. if 
+    // you wouldnt have these await tx.wait() you could process tx three before having processed or made state changes from tx one and two.
 
     // did you win? Check the transaction receipt!
     // if you did, it will be in both the logs and events array
